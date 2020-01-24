aeb6e2929b2352e373c89cc4982fe87ad64883dc
BrianBplusplus <brian@nieuwenhuijzen.net>
2020-01-24 23:46:58 +0100

    Added like/dislike feature to buttons

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 
 export default class Quote extends Component {
+  state = {
+    class: ""
+  };
+
+  handleLike = () => {
+    console.log("handleLike has been pressed");
+    this.setState({
+      class: "liked"
+    });
+  };
+
+  handleDislike = () => {
+    console.log("handleDislike has been pressed");
+    this.setState({
+      class: "disliked"
+    });
+  };
+
   render() {
     return (
-      <div>
+      <div className={this.state.class}>
         <p>"{this.props.text}"</p>
         <p>By: {this.props.author}</p>
-        <button className="likebuttons">
-          <i class="far fa-thumbs-up"></i>
+        <button onClick={this.handleLike} className="likebuttons">
+          <i className="far fa-thumbs-up"></i>
         </button>
-        <button className="likebuttons">
-          <i class="far fa-thumbs-down"></i>
+        <button onClick={this.handleDislike} className="likebuttons">
+          <i className="far fa-thumbs-down"></i>
         </button>
       </div>
     );
